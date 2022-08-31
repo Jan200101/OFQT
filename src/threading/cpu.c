c90a2c9ea1644507c0993513d1fc7925e19b4ea8
Jan200101 <sentrycraft123@gmail.com>
2022-08-31 13:30:01 +0200

    windows implementation for get_core_count()

--- src/threading/cpu.c
+++ src/threading/cpu.c
@@ -1,6 +1,17 @@
+#ifdef _WIN32
+#include <windows.h>
+#else
 #include <sys/sysinfo.h>
+#endif
 
 int get_core_count()
 {
-	return get_nprocs();
+#ifdef _WIN32
+    SYSTEM_INFO sysinfo;
+    GetSystemInfo( &sysinfo );
+
+    return sysinfo.dwNumberOfProcessors;
+#else
+    return get_nprocs();
+#endif
 }
